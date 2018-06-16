38006cc0b258500b3856583ec496bc99e0e09336
Philippe Recto <precto1285@gmal.com>
2018-06-15 20:34:51 -0700

    fixed index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var app = express();
 
 app.use(logger("dev"));
 
-app.use(body.Parser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/");
@@ -61,7 +61,7 @@ app.get("/articles", function(req, res) {
 app.get("/articles/:id", function(req, res) {
     db.Note.create(req.body)
     .then(function(dbNote) {
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: db.Note._id }, { new:true });
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new:true });
     })
     .then(function(dbArticle) {
         res.json(dbArticle);
