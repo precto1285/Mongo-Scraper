7fde510c39a9d86fa556d53f9ee6419931777180
Philippe Recto <precto1285@gmal.com>
2018-06-15 21:27:42 -0700

    updated public and server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var logger = require("morgan");
+var logger = require("logger");
 var mongoose = require("mongoose");
 
 var axios = require("axios");
